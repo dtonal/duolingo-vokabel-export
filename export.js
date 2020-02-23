df301f33da847d93a52e77c23c1830eea9c624cc
dtonal <dtonal@posteo.de>
2020-02-23 10:45:42 +0100

    first commit

---
+++ export.js
@@ -0,0 +1,9 @@
+var x = document.getElementsByClassName('X6or4');
+var i;
+for (i = 0; i < x.length; i++) {
+    var firstElement = x.getElementsByClassName = x[i];
+    var holder = firstElement.getElementsByClassName('_2QLEC')[0];
+    var target = holder.children[0].children[0].children[0].children[0].children[1].children[0].children[0].innerText;
+    var source = holder.children[1].children[0].children[0].children[0].children[1].children[0].children[0].innerText;
+    console.log(source + '||' + target + '|||||');
+}
